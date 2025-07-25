c413b614f4ab0e52525b370590144551520dcb4e
Matthew Heun <matthew.heun@me.com>
2025-07-25 11:50:14 -0400

    Now reallocating!

--- R/reallocate_stat_diffs.R
+++ R/reallocate_stat_diffs.R
@@ -1,4 +1,22 @@
 reallocate <- function(PSUT) {
   PSUT |>
-    Recca::reallocate_statistical_differences()
+    Recca::reallocate_statistical_differences() |>
+    dplyr::mutate(
+      # Get rid of old columns
+      R = NULL,
+      U = NULL,
+      V = NULL,
+      Y = NULL,
+      U_feed = NULL,
+      U_EIOU = NULL
+    ) |>
+    dplyr::rename(
+      # Rename new columns
+      R = R_prime,
+      U = U_prime,
+      V = V_prime,
+      Y = Y_prime,
+      U_feed = U_feed_prime,
+      U_EIOU = U_EIOU_prime
+    )
 }
