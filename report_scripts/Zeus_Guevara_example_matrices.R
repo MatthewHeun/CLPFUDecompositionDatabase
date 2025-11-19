7aae895a5237e0177a42972cb5ba360c4aa0115f
Matthew Heun <matthew.heun@me.com>
2025-11-19 09:49:28 -0500

    Added new reports for Zeus.

--- report_scripts/Zeus_Guevara_example_matrices.R
+++ report_scripts/Zeus_Guevara_example_matrices.R
@@ -3,7 +3,7 @@
 # These are for Zeus Guevara's work to prepare his decomposition
 # code for the data.
 
-data_for_zeus <- tar_read(PSUTReallocated) |>
+data_for_zeus <- targets::tar_read(PSUTReallocated) |>
   dplyr::filter(Country %in% c("MEX", "GHA"), Year %in% 2015:2020) |>
   dplyr::mutate(
     WorksheetNames = paste(Country, Year, sep = "_")
@@ -12,32 +12,60 @@ data_for_zeus <- tar_read(PSUTReallocated) |>
 
 data_for_zeus |>
   Recca::write_ecc_to_excel(path = "~/Desktop/For Zeus/ReallocatedEECs.xlsx",
-                            worksheet_names = "WorksheetNames")
+                            worksheet_names = "WorksheetNames",
+                            overwrite_file = TRUE,
+                            overwrite_worksheets = TRUE)
 
 data_for_zeus |>
   saveRDS(file = "~/Desktop/For Zeus/ReallocatedECCs.rds")
 
-ReallocatedECCs <- readRDS("~/Desktop/For Zeus/ReallocatedECCs.rds")
-ReallocatedECCs$R[[1]] |> as.matrix() |> View()
+# ReallocatedECCs <- readRDS("~/Desktop/For Zeus/ReallocatedECCs.rds")
+# ReallocatedECCs$R[[1]] |> as.matrix() |> View()
 
 # Now also gather the allocation matrices and
 # efficiency vectors for the same years.
+
+# Write allocation (C) matrices to a file
 
 Cmats <- PFUPipelineTools::pl_filter_collect("Cmats",
                                               Country %in% c("MEX", "GHA"),
                                               Year %in% 2015:2020,
                                               collect = TRUE,
                                               conn = PFUPipelineTools::get_mexerdb_conn(user = "dbcreator")) |>
+  tidyr::pivot_longer(cols = c("C_EIOU", "C_Y"), names_to = "matname", values_to = "matrix") |>
   dplyr::mutate(
-    rowsumsCY = matsbyname::rowsums_byname(C_Y),
-    rowsumsCEIOU = matsbyname::rowsums_byname(C_EIOU),
-    WorksheetNames = paste(Country, EnergyType, Year, sep = "_")
-  )
+    matname = factor(matname, levels = c("C_Y", "C_EIOU")),
+    rowsums = matsbyname::rowsums_byname(matrix),
+    WorksheetNames = paste(matname, Country, EnergyType, Year, sep = "_")
+  ) |>
+  dplyr::arrange(matname, Country, Year)
+
+Cmats |>
+  matsindf::write_mats_to_excel(mat_colname = "matrix",
+                                worksheet_names = "WorksheetNames",
+                                path = "~/Desktop/For Zeus/Cmats.xlsx")
+
+Cmats |>
+  saveRDS(file = "~/Desktop/For Zeus/Cmats.rds")
+
+
+# Write efficiency (etafu) vectors to a file
 
 etafuvecs <- PFUPipelineTools::pl_filter_collect("Etafuvecs",
                                                  Country %in% c("MEX", "GHA"),
                                                  Year %in% 2015:2020,
                                                  collect = TRUE,
-                                                 conn = PFUPipelineTools::get_mexerdb_conn(user = "dbcreator"))
+                                                 conn = PFUPipelineTools::get_mexerdb_conn(user = "dbcreator")) |>
+  dplyr::mutate(
+    WorksheetNames = paste("eta", Country, EnergyType, Year, sep = "_")
+  ) |>
+  dplyr::arrange(Country, Year)
 
 
+etafuvecs |>
+  matsindf::write_mats_to_excel(mat_colname = "etafu",
+                                worksheet_names = "WorksheetNames",
+                                path = "~/Desktop/For Zeus/etafuvecs.xlsx")
+
+etafuvecs |>
+  saveRDS(file = "~/Desktop/For Zeus/etafuvecs.rds")
